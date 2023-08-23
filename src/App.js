dbb9272c1c454a3dfa932e37fc09703429f24a2e
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-08-23 22:55:13 +0300

    home route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
