ba9740db1d4ef3656121520e2164b7f77554233b
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-08-26 22:09:03 +0300

    navlink

--- src/Components/ComponentNavBar.jsx
+++ src/Components/ComponentNavBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function ComponentNavBar() {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/html">HTML</Link>
-      <Link to="/css">CSS</Link>
-      <Link to="/javascript">JavaScript</Link>
-      <Link to="/react">React</Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/html">HTML</NavLink>
+      <NavLink to="/css">CSS</NavLink>
+      <NavLink to="/javascript">JavaScript</NavLink>
+      <NavLink to="/react">React</NavLink>
     </nav>
   );
 }
