5b52a3cfabb566a0524b5cf2ec9d0bff6c00ba1f
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-08-27 15:25:39 +0300

    lazy and suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import { Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import "./App.css";
-import Home from "./Pages/Home";
-import CssInfo from "./Pages/CssInfo";
-import JavaScriptInfo from "./Pages/JavaScript";
-import ReactInfo from "./Pages/React";
-import NotFound from "./Pages/NotFound";
-import Layout from "./Layout/Layout";
-import HTMLInfo from "./Pages/HTMLInfo";
+
+// import Home from "./Pages/Home";
+// import CssInfo from "./Pages/CssInfo";
+// import JavaScriptInfo from "./Pages/JavaScript";
+// import ReactInfo from "./Pages/React";
+// import NotFound from "./Pages/NotFound";
+// import Layout from "./Layout/Layout";
+// import HTMLInfo from "./Pages/HTMLInfo";
+
+const Home = lazy(() => import("./Pages/Home"));
+const CssInfo = lazy(() => import("./Pages/CssInfo"));
+const JavaScriptInfo = lazy(() => import("./Pages/JavaScript"));
+const ReactInfo = lazy(() => import("./Pages/React"));
+const NotFound = lazy(() => import("./Pages/NotFound"));
+const Layout = lazy(() => import("./Layout/Layout"));
+const HTMLInfo = lazy(() => import("./Pages/HTMLInfo"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="html" element={<HTMLInfo />} />
-        <Route path="css" element={<CssInfo />} />
-        <Route path="javascript" element={<JavaScriptInfo />} />
-        <Route path="react" element={<ReactInfo />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="html" element={<HTMLInfo />} />
+          <Route path="css" element={<CssInfo />} />
+          <Route path="javascript" element={<JavaScriptInfo />} />
+          <Route path="react" element={<ReactInfo />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
