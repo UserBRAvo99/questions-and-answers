ee663a5caffe49f65415a97b9495bc8cbad44dc0
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-08-27 21:23:11 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import "./App.css";
 
-import Home from "./Pages/Home";
-import CssInfo from "./Pages/CssInfo";
-import JavaScriptInfo from "./Pages/JavaScript";
-import ReactInfo from "./Pages/React";
-import NotFound from "./Pages/NotFound";
-import Layout from "./Layout/Layout";
-import HTMLInfo from "./Pages/HTMLInfo";
+// import Home from "./Pages/Home";
+// import CssInfo from "./Pages/CssInfo";
+// import JavaScriptInfo from "./Pages/JavaScript";
+// import ReactInfo from "./Pages/React";
+// import NotFound from "./Pages/NotFound";
+// import Layout from "./Layout/Layout";
+// import HTMLInfo from "./Pages/HTMLInfo";
 
-// const Home = lazy(() => import("./Pages/Home"));
-// const CssInfo = lazy(() => import("./Pages/CssInfo"));
-// const JavaScriptInfo = lazy(() => import("./Pages/JavaScript"));
-// const ReactInfo = lazy(() => import("./Pages/React"));
-// const NotFound = lazy(() => import("./Pages/NotFound"));
-// const Layout = lazy(() => import("./Layout/Layout"));
-// const HTMLInfo = lazy(() => import("./Pages/HTMLInfo"));
+const Home = lazy(() => import("./Pages/Home"));
+const CssInfo = lazy(() => import("./Pages/CssInfo"));
+const JavaScriptInfo = lazy(() => import("./Pages/JavaScript"));
+const ReactInfo = lazy(() => import("./Pages/React"));
+const NotFound = lazy(() => import("./Pages/NotFound"));
+const Layout = lazy(() => import("./Layout/Layout"));
+const HTMLInfo = lazy(() => import("./Pages/HTMLInfo"));
 
 function App() {
   return (
